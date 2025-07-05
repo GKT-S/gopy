438ee6640905b4b856f45816fe3e2531234d22de
gök <gok>
2025-07-06 00:14:48 +0300

    Initial Commit

---
+++ Gopy/GopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GopyApp.swift
+//  Gopy
+//
+//  Created by Göktuğ Şahin on 6.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct GopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GopyTests/GopyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GopyTests.swift
+//  GopyTests
+//
+//  Created by Göktuğ Şahin on 6.07.2025.
+//
+
+import Testing
+@testable import Gopy
+
+struct GopyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
